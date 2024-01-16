f353df60aa987834889f6803681b581bd6cbdddc
zl <837647279@qq.com>
2024-01-16 22:45:56 +0800

    测试setup（）

--- src/views/demo/Demo.vue
+++ src/views/demo/Demo.vue
@@ -1,25 +1,30 @@
 <template>
   <button @click="count++">{{ count }}</button>
-  {{title}}
+  {{ title }}
 </template>
 
 <script>
-import {ref} from  'vue'
- export default{
-  props:{
-    title:String
+import { ref } from 'vue'
+export default {
+  props: {
+    title: String
   },
 
-  setup(props){
-    const count=ref(0)
+  setup(props,{attrs,slots,emit,expose}) {
+    const count = ref(0)
     console.log(props.title);
+    console.log(attrs);//透传 Attributes（非响应式的对象，等价于 $attrs）
+    console.log(slots);//插槽（非响应式的对象，等价于 $slots）
+    console.log(emit);//触发事件（函数，等价于 $emit）
+    console.log(expose);//暴露公共属性（函数）
+
     return {
       count
     }
   },
 
-  mounted(){
+  mounted() {
     console.log(this.count);
   }
- }
+}
 </script>
